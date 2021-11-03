a8502581f78b0efa096ffd89c91202e5fa8d0cac
Aslon <70798072+aslon1213@users.noreply.github.com>
2021-11-03 18:29:51 +0500

    Update and rename kjhkjh.cpp to Main_banking_file.cpp

---
+++ Main_banking_file.cpp
@@ -0,0 +1,12 @@
+// Ballar mana shhu faylga yoziladi kodlariz
+// Ertaga qanaqa ishlash kerakligini tushuntirishga harakat qilaman
+
+
+
+// Banking system 
+// banking system 
+
+
+int main() {
+    cout << "H" << endl;
+}
