50fea9507ae8999cb8d265feabc48e278026697d
1nodir <xudoyberdiyevnodirbek04032004@gmail.com>
2021-11-09 15:39:39 +0500

    commit

--- main_.cpp
+++ main_.cpp
@@ -1,33 +1,8 @@
-// Ballar mana shhu faylga yoziladi kodlariz
-// Ertaga qanaqa ishlash kerakligini tushuntirishga harakat qilaman
-
-
-
-// Banking system 
-// banking system 
-
 #include <iostream>
 using namespace as std;
 
 
 int main() {
-    cout << "H" << endl;
-    cout << "ASDASLKD" << endl;
-    //asdajksdlkasd
-    //asdas
-    //Asdas//sdas
-
-    //asdkajsdlkasld
-    // ASlon
-    cout << "Aslon" << "hamidov" << endl;
-    cout << "Khabibullok" << "Khallok" << endl;
-    //s
-
-
-int hhh() {
-    cout << 'asdnbasd' << endl;
-}
     
-        
 }
 
