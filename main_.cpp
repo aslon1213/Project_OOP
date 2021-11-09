33740c7fae59eebee46b7d06b150dcebfd709407
1nodir <xudoyberdiyevnodirbek04032004@gmail.com>
2021-11-09 15:35:02 +0500

    ajshdkjasd

---
+++ main_.cpp
@@ -0,0 +1,28 @@
+// Ballar mana shhu faylga yoziladi kodlariz
+// Ertaga qanaqa ishlash kerakligini tushuntirishga harakat qilaman
+
+
+
+// Banking system 
+// banking system 
+
+#include <iostream>
+using namespace as std;
+
+
+int main() {
+    cout << "H" << endl;
+    cout << "ASDASLKD" << endl;
+    //asdajksdlkasd
+    //asdas
+    //Asdas//sdas
+
+    //asdkajsdlkasld
+    // ASlon
+    cout << "Aslon" << "hamidov" << endl;
+    cout << "Khabibullok" << "Khallok" << endl;
+    //s
+    
+        
+}
+
