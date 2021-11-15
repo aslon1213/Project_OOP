44c6bf06846ef31166729ae2259e868da2b63502
aslon1213 <you@example.com>
2021-11-15 14:06:54 +0500

    mjjj

--- Main_banking_file.cpp
+++ Main_banking_file.cpp
@@ -8,24 +8,41 @@
 
 #include <iostream>
 using namespace std;
+using std:: left;
+using std:: right;
 
 #include <fstream>
 using std::ofstream;
+using std:: ifstream;
 
+#include <iomanip>
+using std::setw;
+using std::setprecision;
+
+int display(char name, double balance);
 
 
 int main() {
     ofstream outClientFile("data.dat", ios::app);
-
+    ifstream inClientFile("data.dat","ios::in");
 
     char name[30];
     double balance;
-    while(cin >> name >> balance ) {
-        outClientFile << name < " " << balance << "" << endl;
+
+    /*while(cin >> name >> balance ) {
+        outClientFile << name << " " << balance << "" << endl;
         cout << " ? ";
-    }    
+    }*/
+    while (inClientFile >> name >> balance >>) {
+        display(name,balance);
+    }
     
+
     return 0;
+}
+
+void display(char name, double balance) {
+    cout << left << setw(10) << account << setw(13) << name << setw(7) << setprecision(2) << right << balance << endl;
 }
 
 
